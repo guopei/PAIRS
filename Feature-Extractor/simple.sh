c0f857bfa833051027e12cba72d1c08a110805f5
Pei <amazingguo@gmail.com>
2019-02-12 12:28:26 -0700

    rename Feature Extractor

--- Feature-Extractor/simple.sh
+++
@@ -1,3 +0,0 @@
-#vanillar resnet on butterfly
-
-CUDA_VISIBLE_DEVICES=4 qlua main.lua -retrain ../models/resnet-50.t7 -data /mv_users/peiguo/dataset/cub-fewshot/full/ -batchSize 16 -dispIter 200 -LR 1e-3 -nClasses 200
